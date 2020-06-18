cd3cf294cb5813dcbd00b941c49306fb2f999210
brunobosca <brunoboscaini1@gmail.com>
2020-06-17 21:35:48 -0300

    primeiroscriptJS

---
+++ meuScript.js
@@ -0,0 +1,8 @@
+let numero1 = Number(window.prompt('Digite o número 1'));
+let numero2 = Number(window.prompt('Digite o número 2'));
+
+if(numero1 > numero2) {
+    alert('O número é maior é ' + numero1)
+} else {
+    alert('O número maior é ' + numero2)
+}
